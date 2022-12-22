4b8e94f5f567dcfcb2317c5587bcec166338d510
Mohammad Mokhtari <118061522+mohammadmokhtari917@users.noreply.github.com>
2022-12-21 23:53:20 -0800

    Creating server by http

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('this is server for Glass Cutting');
+
+const http = require('http');
+
+http.createServer((req, res) => {
+
+
+
+
+
+
+}).listen(80, () => console.log("server start"));
